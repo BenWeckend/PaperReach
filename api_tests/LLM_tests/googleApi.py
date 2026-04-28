ba342b7910d67463a12c551f22033842308bf012
Ben <ben.weckend@web.de>
2026-04-28 20:59:42 +0200

    groq

--- api_tests/LLM_tests/googleApi.py
+++ api_tests/LLM_tests/googleApi.py
@@ -1,8 +1,10 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyDJxEujECe-sLAZZhKHjfoLZtISvfD_jvI")
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
 
 response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents="Give me as a output only 10 two to three word sentences, nothing more. Each text represents the following text as best as possible. The sentances are what could be used as part of a title of a Paper: Daniel Stenberg, der Programmierer von cURL, begann 1997 ein Programm zu schreiben, das IRC-Teilnehmern Daten über Wechselkurse zur Verfügung stellen sollte, welche von Webseiten abgerufen werden mussten. Er setzte dabei auf das vorhandene Open-Source-Tool httpget. Nach einer Erweiterung um andere Protokolle wurde das Programm am 20. März 1998 als cURL 4 erstmals veröffentlicht. Ursprünglich stand der Name für „see URL“ und wurde erst später von Stenberg nach einem besseren Vorschlag zum aktuellen Backronym umgedeutet."
+    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
 )
 print(response.text)
+
